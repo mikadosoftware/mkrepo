11fb823a88eea1a2fad4981cc35660d1389bbe0a
Paul R Brian <paul@mikadosoftware.com>
2019-07-09 07:59:08 +0100

    make test run workable from command line

--- mkrepo/mkrepo.py
+++ mkrepo/mkrepo.py
@@ -1,16 +1,15 @@
 #! -*- coding:utf-8 -*-
-"""
-Designed to mkae simplified repo framework
+"""Designed to mkae simplified repo framework
 I suspect I will be making a lot of them
 
 There are 2 kinds of repo I suspect
 
 * a microservice - that expects to have dockerfiles, jenkins etc
-* a developer (CLI) tool - probably just something like todoinator. Does one thing. Adequately.
-  Will be just the README and help file in the one module I suspect.
-  Needs a deploy means in setup
 
-# TODO: idempotent - can be run over any dir, and will create only missing parts
+* a developer (CLI) tool - probably just something like
+  todoinator. Does one thing. Adequately.  Will be just the README and
+  help file in the one module I suspect.  Needs a deploy means in
+  setup
 
 """
 
@@ -19,12 +18,13 @@ from docopt import docopt
 import shutil
 import os
 import logging
+
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 log.addHandler(ch)
-#to be set carefully..
-#how to set in tests???
+# to be set carefully..
+# how to set in tests???
 confd = {}
 
 DOCOPT = """Make a repo for python
@@ -41,45 +41,50 @@ Options:
 
 """
 
+
 def mk_dir_cli():
     dirs = []
-    files = ['version', 'setup.py', '.gitignore']
+    files = ["version", "setup.py", ".gitignore"]
+
 
 def mk_dir_uservice():
     dirs = []
-    files = ['setup.py',]
+    files = ["setup.py"]
+
 
 def get_template(templatename):
     # read from our location
-    with open(os.path.join(confd['templatedir'], templatename), encoding="utf-8") as fo:
+    with open(os.path.join(confd["templatedir"], templatename), encoding="utf-8") as fo:
         template = fo.read()
     return template
+
 
 def write_file(filename, text):
     """ """
     if not os.path.isabs(filename):
-        pth = os.path.join(confd['rootdir'], filename)
+        pth = os.path.join(confd["rootdir"], filename)
     else:
         pth = filename
     if os.path.isfile(pth):
         log.warning("File %s exists - will not overwrite", pth)
     else:
-        if confd['dryrun']:
+        if confd["dryrun"]:
             log.info("Dry Run - would have written %s", pth)
         else:
             log.info("Writing file %s", pth)
-            with open(pth, 'w', encoding="utf-8") as fo:
+            with open(pth, "w", encoding="utf-8") as fo:
                 fo.write(text)
+
 
 def dir_maker(dirpath):
     """proxy for os.makedirs"""
-    if confd['dryrun']:
+    if confd["dryrun"]:
         log.info("Dry Run - would have mkdir %s", dirpath)
     else:
         log.info("Make dir %s", dirpath)
         os.makedirs(dirpath, exist_ok=True)
-            
-    
+
+
 def mk_dir(rootdir):
     """If not existing, create rootdir
 
@@ -92,64 +97,84 @@ def mk_dir(rootdir):
 
     """
     dir_maker(rootdir)
-    dir_maker(os.path.join(rootdir, confd['pkgname']))
-    write_file(os.path.join(rootdir, confd['pkgname'], '__init__.py'), '')
-    
+    dir_maker(os.path.join(rootdir, confd["pkgname"]))
+    write_file(os.path.join(rootdir, confd["pkgname"], "__init__.py"), "")
+
+
 ##############################################################
 
+
 def mk_setup():
-    template = get_template('setup.py')
-    text = template.format(pkgname=confd['pkgname'])
-    write_file('setup.py', text)
+    template = get_template("setup.py")
+    text = template.format(pkgname=confd["pkgname"])
+    write_file("setup.py", text)
+
 
 def mk_version():
-    write_file('VERSION', '0.0.0')
-    
+    write_file("VERSION", "0.0.0")
+
+
 def mk_license():
-    template = get_template('LICENSE')
-    write_file('LICENSE', template)
+    template = get_template("LICENSE")
+    write_file("LICENSE", template)
+
 
 def mk_authors():
-    write_file('AUTHORS', 'Paul Brian <paul@mikadosoftware.com>')
+    write_file("AUTHORS", "Paul Brian <paul@mikadosoftware.com>")
+
 
 def mk_contributing():
-    template = get_template('CONTRIBUTING')
-    write_file('CONTRIBUTING', template)
+    template = get_template("CONTRIBUTING")
+    write_file("CONTRIBUTING", template)
+
 
 def mk_dockerfile():
-    write_file('Dockerfile', 'FROM ubuntu:latest')
+    write_file("Dockerfile", "FROM ubuntu:latest")
+
 
 def mk_jenkinsfile():
-    write_file('Jenkinsfile', 'TBD')
+    write_file("Jenkinsfile", "TBD")
+
 
 def mk_manifest():
-    write_file('MANIFEST.in', 'include LICENSE')
+    write_file("MANIFEST.in", "include LICENSE")
+
 
 def mk_make():
-    write_file('Makefile', 'TBD')
-    
+    write_file("Makefile", "TBD")
+
+
 def mk_docs():
-    dir_maker(os.path.join(confd['rootdir'], 'docs'))
+    dir_maker(os.path.join(confd["rootdir"], "docs"))
+
 
 def mk_tests():
-    dir_maker(os.path.join(confd['rootdir'], 'tests'))
-    
+    dir_maker(os.path.join(confd["rootdir"], "tests"))
+
+
 def mk_readme():
-    write_file('README.rst',
-               '{pkgname}\n{underline}\n'.format(pkgname=confd['pkgname'],
-                                                 underline='='*len(confd['pkgname'])))
+    write_file(
+        "README.rst",
+        "{pkgname}\n{underline}\n".format(
+            pkgname=confd["pkgname"], underline="=" * len(confd["pkgname"])
+        ),
+    )
+
 
 def mk_pylint():
-    template = get_template('.pylintrc')
-    write_file('.pylintrc', template)
-    
+    template = get_template(".pylintrc")
+    write_file(".pylintrc", template)
+
+
 def mk_gitignore():
-    template = get_template('.gitignore')
-    write_file('.gitignore', template)
+    template = get_template(".gitignore")
+    write_file(".gitignore", template)
+
 
 def mk_requirements():
-    write_file('requirements.txt', '# populate as needed')
-    
+    write_file("requirements.txt", "# populate as needed")
+
+
 ###############################################################
 
 
@@ -159,42 +184,36 @@ def mktitle(txt):
     >>> mktitle("project")
     'project'
     >>> mktitle("pyproject")
-    'pyProject'
+    'pyproject'
  
     """
-    if txt[:2] == 'py':
-        tmp = txt[2:].title()
-        title = 'py' + tmp
-    else:
-        title = txt.lower()
+    title = txt.lower()
     return title
-    
+
 
 def run(args):
-    paramd = {'rootdir': None,
-              'pkgname': None,
-              'dryrun' : None,
-              'templatedir':None
-             }
-
-    parentdir = args['<rootdir>']
-    paramd['pkgname'] = args['<pkgname>'].lower()
-    pkgtitle = mktitle(paramd['pkgname'])
-    paramd['rootdir'] = os.path.abspath(os.path.join(parentdir, pkgtitle))
-    paramd['dryrun'] = args['--dryrun']
-    paramd['templatedir'] = os.path.join(os.path.dirname(
-                                             os.path.abspath(__file__)),
-                                             'templates')
     global confd
-    confd = paramd
-
-    if args['--test']:
+    
+    if args["--test"]:
+        confd['dryrun'] = False
         runtests()
     else:
+        paramd = {"rootdir": None, "pkgname": None, "dryrun": None, "templatedir": None}
+        parentdir = args["<rootdir>"]
+        paramd["pkgname"] = args["<pkgname>"].lower()
+        pkgtitle = mktitle(paramd["pkgname"])
+        paramd["rootdir"] = os.path.abspath(os.path.join(parentdir, pkgtitle))
+        paramd["dryrun"] = args["--dryrun"]
+        paramd["templatedir"] = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "templates"
+        )
+
+        confd = paramd
         doall()
-        
+
+
 def doall():
-    mk_dir(confd['rootdir'])
+    mk_dir(confd["rootdir"])
     mk_setup()
     mk_version()
     mk_license()
@@ -210,13 +229,17 @@ def doall():
     mk_docs()
     mk_tests()
 
+
 def runtests():
     import doctest
+
     doctest.testmod()
+
 
 def main():
     args = docopt(DOCOPT)
     run(args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
